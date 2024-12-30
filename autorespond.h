0a15100634b25abe54c00a0bcd582e9b554b7a54
sagredo-dev <roberto.puzzanghera@sagredo.eu>
2024-12-30 14:50:32 +0100

    v1.2.23

---
+++ autorespond.h
@@ -0,0 +1,12 @@
+#include <time.h>
+
+void addautorespond();
+void addautorespondnow();
+void count_autoresponders();
+void delautorespond();
+void delautorespondnow();
+void modautorespond();
+void modautorespondnow();
+void show_autoresponders(char *user, char *dom, time_t mytime);
+void show_autorespond_line(char *user, char *dom, time_t mytime, char *dir);
+void show_autoresponder(char *ActionUser, char *Domain);
